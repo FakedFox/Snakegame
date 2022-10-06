fd14b3d215778afe14868c1c99201d7f962f3f46
FakedFox <mtimofeev111@gmail.com>
2022-10-06 18:21:52 +0300

    Update settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -1,7 +1,7 @@
 import random
 
-width = 500            # ширина игрового окна
-height = 500           # высота игрового окна
+width = 800                    # ширина игрового окна
+height = 800                   # высота игрового окна
 
 game_speed = 80                # Скорость змеи
 block = 20                     # размер сетки
@@ -14,11 +14,8 @@ grid = (200, 200, 200)         # цвет сетки игрового поля
 running = True                 # состоние игры в процессе\ выход
 fps = 30                       # ФПС
 
-snake_posX = width / block * (block / 2)        # положение змеи по Х
+snake_posX = width / block * (block / 2)       # положение змеи по Х
 snake_posY = height / block * (block / 2)      # положение змеи по У
 print(snake_posY, snake_posX)
 apple_posX = random.randrange(20, width - block, block)  # положение яблока по Х
 apple_posY = random.randrange(20, height - block, block) # положение яблока по У
-
-
-20 / 10 * 5
